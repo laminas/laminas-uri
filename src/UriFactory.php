a7dd1d2a8451798c76a06e744651490d8fcb114b
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:55:33 -0600

    Rewriting as Laminas Project package

--- src/UriFactory.php
+++ src/UriFactory.php
@@ -1,13 +1,12 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-uri for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-uri/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-uri/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Uri;
+namespace Laminas\Uri;
 
 
 /**
@@ -28,12 +27,12 @@ abstract class UriFactory
      * @var array
      */
      protected static $schemeClasses = array(
-        'http'   => 'Zend\Uri\Http',
-        'https'  => 'Zend\Uri\Http',
-        'mailto' => 'Zend\Uri\Mailto',
-        'file'   => 'Zend\Uri\File',
-        'urn'    => 'Zend\Uri\Uri',
-        'tag'    => 'Zend\Uri\Uri',
+        'http'   => 'Laminas\Uri\Http',
+        'https'  => 'Laminas\Uri\Http',
+        'mailto' => 'Laminas\Uri\Mailto',
+        'file'   => 'Laminas\Uri\File',
+        'urn'    => 'Laminas\Uri\Uri',
+        'tag'    => 'Laminas\Uri\Uri',
     );
 
     /**
@@ -84,7 +83,7 @@ abstract class UriFactory
      * @param  string $uriString
      * @param  string $defaultScheme
      * @throws Exception\InvalidArgumentException
-     * @return \Zend\Uri\Uri
+     * @return \Laminas\Uri\Uri
      */
     public static function factory($uriString, $defaultScheme = null)
     {
@@ -112,7 +111,7 @@ abstract class UriFactory
             $uri = new $class($uri);
             if (! $uri instanceof UriInterface) {
                 throw new Exception\InvalidArgumentException(sprintf(
-                    'class "%s" registered for scheme "%s" does not implement Zend\Uri\UriInterface',
+                    'class "%s" registered for scheme "%s" does not implement Laminas\Uri\UriInterface',
                     $class,
                     $scheme
                 ));
