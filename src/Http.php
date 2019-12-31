d5bd9d249700638cbd729991e237b6155ff05f6b
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:55:38 -0600

    Rewriting as Laminas Project package

--- src/Http.php
+++ src/Http.php
@@ -1,13 +1,12 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-uri for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-uri/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-uri/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Uri;
+namespace Laminas\Uri;
 
 /**
  * HTTP URI handler
@@ -163,7 +162,7 @@ class Http extends Uri
      * If no port is set, will return the default port according to the scheme
      *
      * @return int
-     * @see    Zend\Uri\Uri::getPort()
+     * @see    Laminas\Uri\Uri::getPort()
      */
     public function getPort()
     {
