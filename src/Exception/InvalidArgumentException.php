8c59d181f3f37eb392b98ca6b0e016f1f71e1273
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:55:17 -0600

    Rewriting as Laminas Project package

--- src/Exception/InvalidArgumentException.php
+++ src/Exception/InvalidArgumentException.php
@@ -1,14 +1,12 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Uri
+ * @see       https://github.com/laminas/laminas-uri for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-uri/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-uri/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Uri\Exception;
+namespace Laminas\Uri\Exception;
 
 class InvalidArgumentException
     extends \InvalidArgumentException
