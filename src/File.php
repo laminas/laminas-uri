e913c46d390f6bb571248e23656ce9e15e235d6c
Matthew Weier O'Phinney <matthew@weierophinney.net>
2021-09-09 08:38:24 -0500

    qa: apply laminas-coding-standard 2.2 rules
    Primarily removes file-level docblocks.
    
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- src/File.php
+++ src/File.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas/laminas-uri for the canonical source repository
- * @copyright https://github.com/laminas/laminas-uri/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-uri/blob/master/LICENSE.md New BSD License
- */
-
 namespace Laminas\Uri;
 
 use function preg_match;
